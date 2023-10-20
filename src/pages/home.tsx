69f43d4bccd853ad43a4f26b2224ac8a077d3ae8
vkzrx <vkzrxw@gmail.com>
2023-10-20 06:46:46 +0200

    chore: clean-up

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,7 @@
-import { Layout } from "../components/layout";
-
 type HomeProps = {
   name: string;
 };
 
 export const HomePage = (props: HomeProps) => {
-  return (
-    <Layout title="Shorty | Home">
-      <h1 class="bg-red-500">Hello {props.name}</h1>
-    </Layout>
-  );
+  return <h1 class="bg-red-500">Hello {props.name}</h1>;
 };
